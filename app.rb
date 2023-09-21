d7f6b15e2522a5e8b3cacf62ee61ad0ff13b4a8c
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-09-21 05:16:03 +0000

    bare bones of omnicalc 3

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:omnicalc3)
+end
+
+get("/umbrella") do
+  erb(:umbrella)
+end
+
+get("/umbrella_submit") do
+  erb(:umbrella_submit)
+end
+
+get("/ai_message") do
+  erb(:ai_message)
+end
+
+get("/ai_chat") do
+  erb(:ai_chat)
 end
